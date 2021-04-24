45bf3b476d219014e5e3c0017e0e29e5685021be
= <=>
2021-04-25 00:07:06 +0300

    2.gun dersi eklendi

---
+++ oopIntro/src/oopIntro/ProductManager.java
@@ -0,0 +1,9 @@
+package oopIntro;
+
+public class ProductManager {
+	
+	public void addToCart(Product product) { //product adresi yollaniyor
+		System.out.println("Sepete Eklendi :"+product.name);
+	}
+
+}
