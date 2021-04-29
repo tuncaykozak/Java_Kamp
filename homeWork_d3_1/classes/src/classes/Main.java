e88bc722ea7fcbfc5c4aeec17b0da44961da959a
tuncaykozak <tuncaykozak@gmail.com>
2021-04-29 21:48:43 +0300

    odevlere devam

---
+++ homeWork_d3_1/classes/src/classes/Main.java
@@ -0,0 +1,39 @@
+package classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//reference type		
+		CustomerManager customerManager = new CustomerManager();
+		CustomerManager customerManager2 = new CustomerManager();
+		customerManager = customerManager2;//ikisi de ayni referansta
+		customerManager.Add();
+		customerManager.Remove();
+		customerManager.Update();
+		
+		
+		//value type
+		int sayi1 =10;
+		int sayi2 =20;
+		
+		sayi2 =sayi1;
+		sayi1 =30;
+		
+		System.out.println(sayi2);
+		
+		
+		//reference type
+		int [] sayilar1 = {1,2,3};
+		int [] sayilar2 = {4,5,6};
+		
+		sayilar2 = sayilar1;
+		sayilar1[0] = 10;
+		
+		System.out.println(sayilar2[0]);
+		
+	}
+
+}
+
+
